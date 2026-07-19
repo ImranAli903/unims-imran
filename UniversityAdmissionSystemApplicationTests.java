7ae4fabb48e13048d194e1ae33d79f68eb1e593c
ImranAli903 <imranpanhwar010@gmail.com>
2026-07-19 12:24:35 +0500

    Add files via upload

---
+++ UniversityAdmissionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package university.admission.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityAdmissionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
